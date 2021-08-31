696674f12e8bc7f06f402c092ce2af2d7feb5409
danascape <danascape@gmail.com>
2021-08-31 09:45:08 +0200

    X00R: Set Soong namespace for device
    Change-Id: Ie31c19f46d669ac87175e8ec5879850073c9f626

--- device.mk
+++ device.mk
@@ -14,6 +14,10 @@ $(call inherit-product, vendor/asus/X00R/X00R-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# Soong Namespace
+PRODUCT_SOONG_NAMESPACES += \
+    device/asus/X00R
+
 # Screen density
 TARGET_SCREEN_HEIGHT := 1440
 TARGET_SCREEN_WIDTH := 720
